02f8f08758af01674a00bac43aa52d6dfec49ed8
ifabregat <ifabregat@fi.ubar.ar>
2024-05-16 19:05:52 -0300

    Endpoint personajes

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,11 @@
 from flask import Flask
 from flask_cors import CORS
+from time import sleep
 
 app = Flask(__name__)
 CORS(app)
 
-from db.characters import get_all_characters
+from db.characters import get_all_characters, get_character_by_id
 
 @app.route("/")
 def home():
@@ -13,3 +14,8 @@ def home():
 @app.route("/characters")
 def all_characters():
     return get_all_characters()
+
+@app.route("/characters/<id>")
+def character_by_id(id):
+    sleep(1)
+    return get_character_by_id(id)
