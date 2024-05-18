44269d55466fa3a358cd5f4582993ed4ae2b5178
ifabregat <ifabregat@fi.ubar.ar>
2024-05-17 22:58:36 -0300

    Agregue endpoint para agregar caractares

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, request
 from flask_cors import CORS
 from time import sleep
 
 app = Flask(__name__)
 CORS(app)
 
-from db.characters import get_all_characters, get_character_by_id, remove_character
+from db.characters import get_all_characters, get_character_by_id, remove_character, add_character
 
 @app.route("/")
 def home():
@@ -23,3 +23,27 @@ def character_by_id(id):
 @app.route("/characters/<id>", methods = ["DELETE"])
 def remove_character_by_id(id):
     return {"success": remove_character(id)}
+
+@app.route("/characters", methods = ["POST"])
+def create_character():
+    name = request.json.get("name")
+    names = request.json.get("names")
+    publisher = request.json.get("publisher")
+    gender = request.json.get("gender")
+    alignment = request.json.get("alignment")
+    image = request.json.get("image")
+    race = request.json.get("race")
+
+    id = int(get_all_characters()[-1]["id"]) + 1
+
+    character = {
+        "id": id,
+        "name": name,
+        "names": names,
+        "publisher": publisher,
+        "gender": gender,
+        "alignment": alignment,
+        "image": image,
+        "race": race
+    }
+    return {"success": add_character(character), "id": id}
